dfb5520889e2cf01fd378a3e20a10e19fc2ef5fa
zt4ff <segunkayode00@gmail.com>
2022-09-20 09:41:41 +0100

    reduce the min score off lighouthouse checks

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,14 @@ module.exports = {
   ci: {
     collect: {
       url: ["https://www.github.com"],
+      assert: {
+        assertions: {
+          "categories:performance": ["warn", { minScore: 0.1 }],
+          "categories:accessibility": ["warn", { minScore: 0.1 }],
+          "categories:seo": ["warn", { minScore: 0.1 }],
+          "categories:best-practices": ["warn", { minScore: 0.1 }],
+        },
+      },
     },
     uploads: {
       target: "temporart-public-storage",
